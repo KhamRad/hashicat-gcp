e0218983ee8ba6e2a11d18aa03b5163625dcd32c
Radik Khamidullin <rhamidullin@mail.ru>
2021-11-04 14:23:48 +0000

    Added VPC module2

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,14 @@
-network_name = gaurav-network
-project_id   = var.project
-subnets = [
-  {
+module "network" {
+  source  = "app.terraform.io/example-org-a1b86e/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  network_name = gaurav-network
+  project_id   = var.project
+  subnets = [
+   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
-  }
-]
+   }
+  ]
+}
